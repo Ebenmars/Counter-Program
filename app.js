d45dbfcf60d8111ff60717bcd436b6c5a660f2e4
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-06-08 21:47:19 -0400

    Completed Logic For Counter

---
+++ app.js
@@ -0,0 +1,25 @@
+function decrease(){
+    let num = document.getElementById("num");
+    let number = parseInt(num.innerHTML);
+    number--;
+    num.innerHTML = number;
+ }
+
+
+
+function reset(){
+    let num = document.getElementById("num");
+    num.innerHTML = 0;
+}
+
+function increase(){
+    let num = document.getElementById("num");
+    let number = parseInt(num.innerHTML);
+    number++;
+    num.innerHTML = number;
+}
+
+
+
+
+  
